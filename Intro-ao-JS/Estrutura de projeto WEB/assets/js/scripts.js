bda404a902047fc108952b4e6b5f350cd320785c
renan-eng <renan3838@gmail.com>
2021-12-06 17:18:41 -0300

    Adicionando um perojeto simples de um contador utilizando JS

--- Intro-ao-JS/Estrutura de projeto WEB/assets/js/scripts.js
+++ Intro-ao-JS/Estrutura de projeto WEB/assets/js/scripts.js
@@ -1,1 +1,12 @@
-console.log("Hello Word!")
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function increment() {
+  currentNumber++;
+  currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+  currentNumber--;
+  currentNumberWrapper.innerHTML = currentNumber;
+}
