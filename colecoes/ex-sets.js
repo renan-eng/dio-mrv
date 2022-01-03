793ed0ee4525c3995d7e5194c68a9240b306482e
renan-eng <renan3838@gmail.com>
2022-01-03 09:28:04 -0300

    Corrigindo a saida da funçao arrUnic() em ex-sets.js para retornar corretamente um ARRAY E NAO SET

--- colecoes/ex-sets.js
+++ colecoes/ex-sets.js
@@ -2,7 +2,7 @@ let arr1 = [30, 30, 40, 5, 223, 2049, 3034, 5];
 
 function arrUnico(arr) {
   let arrUnico = new Set(arr);
-  return arrUnico;
+  return [...arrUnico];
 }
 
 console.log(arrUnico(arr1));
