2a157841b76664d097a570e9943a025ed79e07a6
renan-eng <renan3838@gmail.com>
2022-01-10 10:35:24 -0300

    Adicionado a funçao que permite o jogador atirar e formatado o codigo usando Prettier

--- jogo-nave/jogo1/js/js.js
+++ jogo-nave/jogo1/js/js.js
@@ -14,8 +14,9 @@ function start() { // Inicio da função start
   //Principais variáveis do jogo
 	
 	var jogo = {};
-  var velocidade=5;
+  var velocidade= 5;
   var posicaoY = parseInt(Math.random() * 334);
+  var podeAtirar= true;
   var TECLA = {
     W: 87,
     S: 83,
@@ -87,7 +88,9 @@ function start() { // Inicio da função start
     
     if (jogo.pressionou[TECLA.D]) {
       
-      //Chama função Disparo	
+      //Chama função Disparo
+      disparo();
+	
     }
   
     } // fim da funçãoo movejogador()
@@ -127,7 +130,40 @@ function start() { // Inicio da função start
             
     }
   
-  } // fim da fun��o moveamigo()
+  } // fim da função moveamigo()
+
+  function disparo() {
+	
+    if (podeAtirar==true) {
+      
+    podeAtirar=false;
+    
+    topo = parseInt($("#jogador").css("top"))
+    posicaoX= parseInt($("#jogador").css("left"))
+    tiroX = posicaoX + 190;
+    topoTiro=topo+37;
+    $("#fundoGame").append("<div id='disparo'></div");
+    $("#disparo").css("top",topoTiro);
+    $("#disparo").css("left",tiroX);
+    
+    var tempoDisparo=window.setInterval(executaDisparo, 30);
+    
+    } //Fecha podeAtirar
+   
+           function executaDisparo() {
+        posicaoX = parseInt($("#disparo").css("left"));
+        $("#disparo").css("left",posicaoX+15); 
+  
+              if (posicaoX>900) {
+              
+        window.clearInterval(tempoDisparo);
+        tempoDisparo=null;
+        $("#disparo").remove();
+        podeAtirar=true;
+            
+       }
+    } // Fecha executaDisparo()
+  } // Fecha disparo()
 
 } // Fim da função start
 
