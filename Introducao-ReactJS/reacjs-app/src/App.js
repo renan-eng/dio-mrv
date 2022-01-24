8b09c1fe6f27e0ea6f139ecdb4d493d4fe228df1
renan-eng <renan3838@gmail.com>
2022-01-24 09:23:45 -0300

    Criada uma função soma para exemplificar como o javascript pode ser inserido dentro do jsx do react

--- Introducao-ReactJS/reacjs-app/src/App.js
+++ Introducao-ReactJS/reacjs-app/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
+function primeiroJSX() {
+  return (
+    <p>Renan Eduardo - Introdução ao ReactJS</p>
+  )}
+
+let soma = (a, b) => {return a + b}
+
 function App() {
   return (
-    <div className="App">
+    <div className="App teste">
       <h1>Hello Word</h1>
+      {primeiroJSX()}
+      <p>Soma: {soma(12,20)}</p>
     </div>
   );
 }
